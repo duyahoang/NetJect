199e2106e7c4d35a65e773486ece7067a830f884
Duy Hoang (duyhoan) <duyhoan@cisco.com>
2023-10-13 11:39:11 -0700

    Update net-cmd-parser + add device-test.txt

--- net-cmd-parser.py
+++ net-cmd-parser.py
@@ -4,9 +4,12 @@ import re
 import json
 import csv
 import argparse
+import logging
 
 
 def parse_nxos_show_version(output):
+    logging.debug('Parsing "show version"...')
+    
     # Define regular expressions for the attributes
     regex_map = {
         'bios_version': r'BIOS: version (.+)',
@@ -39,6 +42,8 @@ def parse_nxos_show_version(output):
 
 
 def parse_nxos_show_interface(output):
+    logging.debug('Parsing "show interface"...')
+
     # Define regular expressions for the attributes
     regex_map = {
         'interface': r'(.+?) is (up|down)',
@@ -86,84 +91,190 @@ def parse_nxos_show_interface(output):
 
 
 def parse_nxos_show_interface_trunk(output):
-    interfaces = re.findall(r'(Eth\d+/\d+|Po\d+)', output)
-    native_vlans = re.findall(r'(\d+)\s+(trunking|trnk-bndl|not-trunking)', output)
-    port_channels = re.findall(r'(trunking|trnk-bndl|not-trunking)\s+(--|Po\d+)', output)
-    vlans_allowed = re.findall(r'(Eth\d+/\d+|Po\d+)\s+(\d+-\d+,\d+-\d+)', output)
-    stp_forwarding = re.findall(r'(Eth\d+/\d+|Po\d+)\s+(none)', output)
+    logging.debug('Parsing "show interface trunk"...')
 
-    result = {'interfaces': {}}
-    for i, interface in enumerate(interfaces):
-        result['interfaces'][interface] = {
-            'native_vlan': native_vlans[i][0],
-            'status': native_vlans[i][1],
-            'port_channel': port_channels[i][1] if port_channels[i][1] != '--' else None,
-            'vlans_allowed': vlans_allowed[i][1],
-            'stp_forwarding': stp_forwarding[i][1]
+    # Regex patterns map
+    regex_map = {
+        'port_details_header': r'Port\s+Native\s+Status\s+Port',
+        'port_details': r'(?P<port>\S+)\s+(?P<native_vlan>\d+)\s+(?P<status>\S+(?:-\S+)?)\s+(?P<port_channel>\S+)',
+        'vlans_allowed_header': r'Port\s+Vlans Allowed on Trunk',
+        'vlans_allowed': r'(?P<port>\S+)\s+(?P<vlans_allowed>[\d,-]+)',
+        'stp_forwarding_header': r'Port\s+STP Forwarding',
+        'stp_forwarding': r'(?P<port>\S+)\s+(?P<stp_forwarding>\S+)'
+    }
+    
+    # Identify the start indices of each section based on headers
+    port_details_index = re.search(regex_map['port_details_header'], output).end()
+    vlans_allowed_index = re.search(regex_map['vlans_allowed_header'], output).end()
+    stp_forwarding_index = re.search(regex_map['stp_forwarding_header'], output).end()
+
+    # Extract section strings
+    port_details_string = output[port_details_index:vlans_allowed_index]
+    vlans_allowed_string = output[vlans_allowed_index:stp_forwarding_index]
+    stp_forwarding_string = output[stp_forwarding_index:]
+    
+    # Parse port details section
+    port_details = {}
+    for match in re.finditer(regex_map['port_details'], port_details_string):
+        port = match.group('port')
+        port_details[port] = {
+            'Native Vlan': match.group('native_vlan'),
+            'Status': match.group('status'),
+            'Port Channel': match.group('port_channel')
         }
+
+    # Parse VLANs allowed section
+    vlans_allowed = {}
+    for match in re.finditer(regex_map['vlans_allowed'], vlans_allowed_string):
+        port = match.group('port')
+        vlans_allowed[port] = match.group('vlans_allowed')
+
+    # Parse STP Forwarding section
+    stp_forwarding = {}
+    for match in re.finditer(regex_map['stp_forwarding'], stp_forwarding_string):
+        port = match.group('port')
+        stp_forwarding[port] = match.group('stp_forwarding')
+
+    # Combine the parsed data into a single dictionary
+    result = {}
+    for port in port_details:
+        result[port] = port_details[port]
+        result[port]['Vlans Allowed on Trunk'] = vlans_allowed.get(port, '')
+        result[port]['STP Forwarding'] = stp_forwarding.get(port, '')
 
     return result
 
 
 def parse_nxos_show_vlan(output):
-    # Define regular expressions for each attribute
+    logging.debug('Parsing "show vlan"...')
+
+    # Regex patterns map
     regex_map = {
-        'vlan_id': re.compile(r'^(\d+)\s+'),
-        'name': re.compile(r'(\S+)\s+(active|suspended)'),
-        'status': re.compile(r'(\S+)\s+(active|suspended)'),
-        'ports': re.compile(r'active\s+(Eth[^\n]+)'),
-        'type': re.compile(r'(\d+)\s+(\w+)\s+'),
-        'mode': re.compile(r'(\d+)\s+\w+\s+(\w+)')
+        'vlan_details': r'(?P<vlan_id>\d+)\s+(?P<vlan_name>\S+)\s+(?P<status>\S+)(?P<ports>[\w\s,/-]*)',
+        'vlan_type_mode': r'(?P<vlan_id>\d+)\s+(?P<type>\S+)\s+(?P<mode>\S+)',
+        'remote_span': r'(?P<primary>\d+)\s+(?P<secondary>\d+)\s+(?P<type>\S+)\s+(?P<ports>[\w\s,/-]*)'
     }
 
-    # Split the output by lines and initialize the vlan dictionary
-    lines = output.split('\n')
-    vlans = {}
-
-    # Loop through each line to find the attributes
-    for line in lines:
-        vlan_id_match = regex_map['vlan_id'].search(line)
-        if vlan_id_match:
-            vlan_id = int(vlan_id_match.group(1))
-            vlans[vlan_id] = {}
-
-        for attr, regex in regex_map.items():
-            match = regex.search(line)
-            if match and vlan_id:
-                if attr == 'ports':
-                    vlans[vlan_id][attr] = [port.strip() for port in match.group(1).split(',')]
-                elif attr != 'vlan_id':
-                    vlans[vlan_id][attr] = match.group(1)
-
-    return vlans
+    # Identify the start indices of each section based on headers
+    vlan_detail_index = output.find('VLAN Name')
+    vlan_type_mode_index = output.find('VLAN Type  Vlan-mode')
+    remote_span_index = output.find('Remote SPAN VLANs')
+    
+    # Extract section strings
+    vlan_detail_string = output[vlan_detail_index:vlan_type_mode_index]
+    vlan_type_mode_string = output[vlan_type_mode_index:remote_span_index]
+    remote_span_string = output[remote_span_index:]
+    
+    vlan_data = {}
+    
+    # Variable to hold current VLAN ID
+    current_vlan = None
+    
+    # Parse VLAN details with ports mapping
+    for line in vlan_detail_string.splitlines():
+        match = re.search(regex_map['vlan_details'], line)
+        if match:
+            current_vlan = match.group('vlan_id')
+            ports_list = match.group('ports').strip().split(', ')
+            # Ensure that empty strings are not included in the Ports list
+            ports_list = [port for port in ports_list if port]
+            vlan_data[current_vlan] = {
+                'VLAN Name': match.group('vlan_name'),
+                'Status': match.group('status'),
+                'Ports': ports_list
+            }
+        elif current_vlan and line.strip():
+            # Continuation lines for the ports of a VLAN
+            ports = line.strip().split(', ')
+            vlan_data[current_vlan]['Ports'].extend(ports)
+    
+    # Parse VLAN type and mode
+    for line in vlan_type_mode_string.splitlines():
+        match = re.search(regex_map['vlan_type_mode'], line)
+        if match:
+            vlan_id = match.group('vlan_id')
+            if vlan_id in vlan_data:  # Ensure the VLAN ID exists in the data
+                vlan_data[vlan_id].update({
+                    'Type': match.group('type'),
+                    'Mode': match.group('mode')
+                })
+    
+    # Parse Remote SPAN VLANs
+    for line in remote_span_string.splitlines():
+        match = re.search(regex_map['remote_span'], line)
+        if match:
+            primary = match.group('primary')
+            # If primary VLAN ID exists in the data, update its attributes
+            if primary in vlan_data:
+                vlan_data[primary].update({
+                    'Secondary': match.group('secondary'),
+                    'RS_Type': match.group('type'),
+                    'RS_Ports': match.group('ports').strip().split(', ')
+                })
+    
+    return vlan_data
 
 
 def parse_nxos_show_interface_status(output):
+    logging.debug('Parsing "show interface status"...')
+    
     regex_map = {
-        'port': re.compile(r'^(Eth\d+/\d+)\s+'),
-        'name': re.compile(r'^\S+\s+([^\s]+)\s+'),
-        'status': re.compile(r'^\S+\s+\S+\s+(\S+)\s+'),
-        'vlan': re.compile(r'^\S+\s+\S+\s+\S+\s+(\S+)\s+'),
-        'duplex': re.compile(r'^\S+\s+\S+\s+\S+\s+\S+\s+(\S+)\s+'),
-        'speed': re.compile(r'^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)\s+'),
-        'type': re.compile(r'^\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)')
+        'interface_status': r'^(?P<port>Eth\d+/\d+)\s+(?P<name>.+?)\s+(?P<status>down|err-disabled|err-vlans|inactive|up|module \d{1,2})\s+(?P<vlan>\S+)\s+(?P<duplex>\S+)\s+(?P<speed>\S+)\s+(?P<type>\S+)$'
     }
 
     interfaces = {}
     lines = output.split('\n')
 
     for line in lines:
-        port_match = regex_map['port'].search(line)
-        if port_match:
-            port = port_match.group(1)
-            interfaces[port] = {}
-
-            for attr, regex in regex_map.items():
-                match = regex.search(line)
-                if match and attr != 'port':
-                    interfaces[port][attr] = match.group(1)
+        match = re.search(regex_map['interface_status'], line)
+        if match:
+            port = match.group('port')
+            interfaces[port] = {
+                'Name': match.group('name'),
+                'Status': match.group('status'),
+                'Vlan': match.group('vlan'),
+                'Duplex': match.group('duplex'),
+                'Speed': match.group('speed'),
+                'Type': match.group('type')
+            }
 
     return interfaces
+
+
+def parse_nxos_show_ip_route_all(output):
+    logging.debug('Parsing "show ip route all"...')
+
+    regex_map = {
+        'route': re.compile(r'^([\d.]+/\d+),\s+(\d+)\s+ucast next-hops, (\d+)\s+mcast next-hops'),
+        'next_hop': re.compile(r'^\s+\*\s*via\s+(\S+),\s+\[([\d/]+)\],\s+(\d+:\d+:\d+),\s+(\w+),\s+(\w+)')
+    }
+
+    routes = {}
+    lines = output.split('\n')
+
+    current_route = None
+    for line in lines:
+        route_match = regex_map['route'].search(line)
+        if route_match:
+            current_route = route_match.group(1)
+            routes[current_route] = {
+                'ucast_next_hops': int(route_match.group(2)),
+                'mcast_next_hops': int(route_match.group(3)),
+                'next_hops': []
+            }
+        elif current_route:
+            next_hop_match = regex_map['next_hop'].search(line)
+            if next_hop_match:
+                next_hop = {
+                    'via': next_hop_match.group(1),
+                    'preference_metric': next_hop_match.group(2),
+                    'age': next_hop_match.group(3),
+                    'source': next_hop_match.group(4),
+                    'type': next_hop_match.group(5)
+                }
+                routes[current_route]['next_hops'].append(next_hop)
+
+    return {"routes": routes}
 
 
 # New IOS parsers (only definition and arguments)
@@ -234,19 +345,46 @@ def parse_ios_show_run(output):
     return {"show run": interfaces}
 
 
-def parse_text_file(filename, command_parsers, address):
+def extract_txt_cmd_output(text, commands):
+    output = {}
+    positions = []
+    
+    # find the positions of each command in the text
+    for cmd in commands:
+        pos = text.find(cmd)
+        if pos != -1:
+            positions.append((pos, cmd))
+    
+    # sort the positions to maintain the order in text
+    positions.sort()
+    
+    # extract the output of each command
+    for i in range(len(positions)):
+        start = positions[i][0] + len(positions[i][1])  # start of the output
+        end = positions[i+1][0] if i+1 < len(positions) else len(text)  # end of the output
+        cmd_output = text[start:end].strip()  # extract the output
+        output[positions[i][1]] = cmd_output
+    
+    return output
+
+
+def parse_text_file(filename, command_parsers):
+    
     with open(filename, 'r') as file:
         content = file.read()
-        commands = re.findall(r'show \w+', content)
+        cmd_output = extract_txt_cmd_output(content, command_parsers.keys())
         outputs = {}
-        for index, command in enumerate(commands):
-            start = content.index(command) + len(command)
-            end = content.index(commands[index + 1]) if index + 1 < len(commands) else None
-            output = content[start:end]
-            parser = command_parsers.get(command)
+        for cmd, output in cmd_output.items():
+            parser = command_parsers.get(cmd)
             if parser:
-                outputs[command] = parser(output)
-        return {address: outputs}
+                outputs[cmd] = parser(output)
+    
+    return {filename: outputs}
+
+
+def write_txt(filename, data):
+    with open(filename, 'w') as f:
+        f.write(data)
 
 
 def write_json(data):
@@ -276,20 +414,21 @@ async def main():
     
     # Argument parser
     parser = argparse.ArgumentParser(description="Network Device Parser")
-    parser.add_argument("os_type", choices=["IOS", "NX-OS"], help="Operating system type of the network device.")
+    parser.add_argument("os_type", choices=["ios", "nxos"], help="Operating system type of the network device.")
     parser.add_argument("--file", help="Text file containing the output of the show commands.")
     args = parser.parse_args()
     
     # Choose the appropriate parsers based on the OS type
-    if args.os_type == "NX-OS":
+    if args.os_type == "nxos":
         command_parsers = {
             'show version': parse_nxos_show_version,
             'show interface': parse_nxos_show_interface,
             'show interface trunk': parse_nxos_show_interface_trunk,
             'show vlan': parse_nxos_show_vlan,
-            'show interface status': parse_nxos_show_interface_status
+            'show interface status': parse_nxos_show_interface_status,
+            'show ip route all': parse_nxos_show_ip_route_all
         }
-    elif args.os_type == "IOS":
+    elif args.os_type == "ios":
         command_parsers = {
             'show version': parse_ios_show_version,
             'show interface': parse_ios_show_interface,
@@ -301,7 +440,7 @@ async def main():
     
     # If a text file is provided, parse the commands from the text file
     if args.file:
-        outputs = parse_text_file(args.file, command_parsers, args.file)
+        outputs = parse_text_file(args.file, command_parsers)
         write_json(outputs)
     else:
         devices = []
